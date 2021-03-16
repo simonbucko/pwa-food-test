76601335fd6aa3a8f0f6729056b7135282d4c93e
simonbucko <bucino36@gmail.com>
2021-03-16 12:18:05 +0100

    initial commit

---
+++ public/js/db.js
@@ -0,0 +1,47 @@
+//offline data
+db.enablePersistence().catch((err) => {
+  if (err.code == "failed-precondition") {
+    //probably multiple tabs open at once
+    console.log("persistance failed");
+  } else if (err.code == "unimplemented") {
+    //lack of browser support
+    console.log("persistance is not available");
+  }
+});
+
+db.collection("recipes").onSnapshot((snapshot) => {
+  snapshot.docChanges().forEach((change) => {
+    if (change.type === "added") {
+      renderRecipe(change.doc.data(), change.doc.id);
+    }
+    if (change.type === "removed") {
+      removeRecipe(change.doc.id);
+    }
+  });
+});
+
+//add new recipe
+const form = document.querySelector("form");
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const recipe = {
+    title: form.title.value,
+    ingredients: form.ingredients.value,
+  };
+  db.collection("recipes")
+    .add(recipe)
+    .catch((err) => {
+      console.log(err);
+    });
+  form.title.value = "";
+  form.ingredients.value = "";
+});
+
+//delete recipe
+const recipeContainer = document.querySelector(".recipes");
+recipeContainer.addEventListener("click", (evt) => {
+  if (evt.target.tagName === "I") {
+    const id = evt.target.getAttribute("data-id");
+    db.collection("recipes").doc(id).delete();
+  }
+});
